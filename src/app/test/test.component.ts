741068bac438dc043539da3807363bae4897ad56
yor1g <yor1g@hotmail.com>
2020-10-06 02:48:18 +0800

    3 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-test',
@@ -7,12 +10,32 @@ import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  @Input()
-  mainForm: TemplateRef<any> | null;
+randomUserUrl = 'https://api.randomuser.me/?results=10';
+  optionList: string[] = [];
+  selectedUser = null;
+  isLoading = false;
+  // tslint:disable:no-any
+  getRandomNameList: Observable<string[]> = this.http
+    .get(`${this.randomUserUrl}`)
+    .pipe(map((res: any) => res.results))
+    .pipe(
+      map((list: any) => {
+        return list.map((item: any) => `${item.name.first}`);
+      })
+    );
 
-  constructor() { }
+  loadMore(): void {
+    this.isLoading = true;
+    this.getRandomNameList.subscribe(data => {
+      this.isLoading = false;
+      this.optionList = [...this.optionList, ...data];
+    });
+  }
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadMore();
   }
 
 }
